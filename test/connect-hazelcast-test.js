fe742e913540d33ebb5c736e8d3eb5e15c9d294e
Andrey Pechkurov <37772591+puzpuzpuz@users.noreply.github.com>
2020-10-05 15:01:20 +0300

    Enhancement/bump client version (#3)

--- test/connect-hazelcast-test.js
+++ test/connect-hazelcast-test.js
@@ -1,3 +1,6 @@
+/* eslint-env mocha */
+'use strict';
+
 const Mocha = require('mocha');
 const describe = Mocha.describe;
 const expect = require('chai').expect;
@@ -6,9 +9,7 @@ const GenericContainer = TestContainers.GenericContainer;
 const expressSession = require('express-session');
 const HazelcastStore = require('../')(expressSession);
 
-let Client = require('hazelcast-client').Client;
-let Config = require('hazelcast-client').Config;
-let config = new Config.ClientConfig();
+const { Client } = require('hazelcast-client');
 
 describe('HazelcastStore', () => {
 
@@ -16,49 +17,187 @@ describe('HazelcastStore', () => {
     let hazelcastClient;
 
     before(async () => {
-        console.info("Starting Hazelcast Cluster...");
-        container = await new GenericContainer("hazelcast/hazelcast", "3.12.8")
+        console.info('Starting Hazelcast Cluster...');
+        container = await new GenericContainer('hazelcast/hazelcast', '4.0.3')
             .withExposedPorts(5701)
             .start();
-        config.networkConfig.addresses.push(`127.0.0.1:${container.getMappedPort(5701)}`);
-        await Client.newHazelcastClient(config).then(function(client) {
-            hazelcastClient = client
+        hazelcastClient = await Client.newHazelcastClient({
+            network: {
+                clusterMembers: [ `127.0.0.1:${container.getMappedPort(5701)}` ]
+            }
         });
     });
 
     after(async () => {
-        console.info("Stopping Hazelcast Cluster...");
+        console.info('Stopping Hazelcast Cluster...');
         await hazelcastClient.shutdown();
         await container.stop();
     });
 
-    it('should set', async () => {
-        let store = new HazelcastStore({client: hazelcastClient});
-        store.set('key', 'val', (data, error) => {
-            expect(data).to.equal(null);
-            expect(error).to.equal(undefined);
+    it('constructor: should throw when client is not provided', () => {
+        expect(() => new HazelcastStore({})).to.throw(Error);
+    });
+
+    it('set: should store session', (done) => {
+        const store = new HazelcastStore({client: hazelcastClient});
+        store.set('keySet', 'val', (error, data) => {
+            if (error) {
+                return done(error);
+            }
+            expect(data).to.equal('OK');
+            expect(error).to.equal(null);
+            done();
         });
     });
 
-    it('should get', async () => {
-        let store = new HazelcastStore({client: hazelcastClient});
-        store.set('key1', 'val1', (data, error) => {
-            store.get('key1', (data, error) => {
-                expect(data).to.equal('val1');
-                expect(error).to.equal(undefined);
+    it('set: should respect ttl option', (done) => {
+        const store = new HazelcastStore({client: hazelcastClient, ttl: 1});
+        store.set('keySetWithTTL1', 'val', (error) => {
+            if (error) {
+                return done(error);
+            }
+            // check that session expires
+            setTimeout(() => {
+                store.get('keySetWithTTL1', (error, data) => {
+                    if (error) {
+                        return done(error);
+                    }
+                    expect(data).to.equal(null);
+                    expect(error).to.equal(null);
+                    done();
+                });
+            }, 2000);
+        });
+    });
+
+    it('set: should respect ttl cookie', (done) => {
+        const store = new HazelcastStore({client: hazelcastClient, ttl: Number.MAX_SAFE_INTEGER});
+        const session = {
+            cookie: {
+                // already expired session
+                expires: (new Date(Date.now() - 3000)).toUTCString()
+            }
+        };
+        store.set('keySetWithTTL2', session, (error) => {
+            if (error) {
+                return done(error);
+            }
+            // check that session expired
+            setTimeout(() => {
+                store.get('keySetWithTTL2', (error, data) => {
+                    if (error) {
+                        return done(error);
+                    }
+                    expect(data).to.equal(null);
+                    expect(error).to.equal(null);
+                    done();
+                });
+            }, 1000);
+        });
+    });
+
+    it('get: should retrieve existing session', (done) => {
+        const store = new HazelcastStore({client: hazelcastClient});
+        store.set('keyGet1', 'val', (error) => {
+            if (error) {
+                return done(error);
+            }
+            store.get('keyGet1', (error, data) => {
+                if (error) {
+                    return done(error);
+                }
+                expect(data).to.equal('val');
+                expect(error).to.equal(null);
+                done();
             });
         });
     });
 
-    it('should destroy', async () => {
-        let store = new HazelcastStore({client: hazelcastClient});
-        store.set('key2', 'val2', (data, error) => {
-            store.destroy('key2', (data, error) => {
-                store.get('key2', (data, error) => {
+    it('get: should return null for non-existing session', (done) => {
+        const store = new HazelcastStore({client: hazelcastClient});
+        store.get('keyGet2', (error, data) => {
+            if (error) {
+                return done(error);
+            }
+            expect(data).to.equal(null);
+            expect(error).to.equal(null);
+            done();
+        });
+    });
+
+    it('destroy: should clear sessions', (done) => {
+        const store = new HazelcastStore({client: hazelcastClient});
+        store.set('keyDestroy', 'val', (error) => {
+            if (error) {
+                return done(error);
+            }
+            store.destroy('keyDestroy', (error) => {
+                if (error) {
+                    return done(error);
+                }
+                store.get('keyDestroy', (error, data) => {
+                    if (error) {
+                        return done(error);
+                    }
                     expect(data).to.equal(null);
-                    expect(error).to.equal(undefined);
+                    expect(error).to.equal(null);
+                    done();
                 });
             });
+        });
+    });
+
+    it('touch: should update ttl when disableTouch=false', (done) => {
+        const store = new HazelcastStore({client: hazelcastClient, ttl: 3});
+        store.set('keyTouch1', 'val', (error) => {
+            if (error) {
+                return done(error);
+            }
+            setTimeout(() => {
+                store.touch('keyTouch1', 'val', (error) => {
+                    if (error) {
+                        return done(error);
+                    }
+                    setTimeout(() => {
+                        // the session should be still present after 4 seconds
+                        store.get('keyTouch1', (error, data) => {
+                            if (error) {
+                                return done(error);
+                            }
+                            expect(data).to.equal('val');
+                            expect(error).to.equal(null);
+                            done();
+                        });
+                    }, 2000);
+                });
+            }, 2000);
+        });
+    });
+
+    it('touch: should not update ttl when disableTouch=true', (done) => {
+        const store = new HazelcastStore({client: hazelcastClient, ttl: 3, disableTouch: true});
+        store.set('keyTouch2', 'val', (error) => {
+            if (error) {
+                return done(error);
+            }
+            setTimeout(() => {
+                store.touch('keyTouch2', 'val', (error) => {
+                    if (error) {
+                        return done(error);
+                    }
+                    setTimeout(() => {
+                        // the session should be gone after 4 seconds
+                        store.get('keyTouch2', (error, data) => {
+                            if (error) {
+                                return done(error);
+                            }
+                            expect(data).to.equal(null);
+                            expect(error).to.equal(null);
+                            done();
+                        });
+                    }, 2000);
+                });
+            }, 2000);
         });
     });
 });
